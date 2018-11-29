1fc1196104d69d2394ddcc76fe06dc5c1d630eef
VINOD <VINOD@VINOD-PC>
2018-11-29 10:48:35 +0530

    coommit and push2

--- InetBankingV11/src/test/java/com/inetbanking/testCases/TC_EditCustomerTest_004.java
+++ InetBankingV11/src/test/java/com/inetbanking/testCases/TC_EditCustomerTest_004.java
@@ -4,4 +4,7 @@ public class TC_EditCustomerTest_004 {
 // This Edit customer test case
 	// This Edit customer test case
 	// This Edit customer test case
+	//This Edit customer test case
+	// This Edit customer test case
+	// This Edit
 }
